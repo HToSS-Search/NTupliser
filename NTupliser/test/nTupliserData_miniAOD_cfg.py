cfb4093e00fc230183bb4e7f6b911370c3f66874
davidcarbonis <alexander.david.morton@cern.ch>
2021-02-09 12:54:53 +0100

    Update

--- NTupliser/test/nTupliserData_miniAOD_cfg.py
+++ NTupliser/test/nTupliserData_miniAOD_cfg.py
@@ -44,7 +44,7 @@ process.options = cms.untracked.PSet(
                      )
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag.globaltag = cms.string('94X_dataRun2_v6') #RunsA-F 80X_dataRun2_2016SeptRepro_v7; RunH 80X_dataRun2_Prompt_v16
+process.GlobalTag.globaltag = cms.string('94X_dataRun2_ReReco_EOY17_v6')
 
 #There's a bit in here about some btau tags that the code looks for. I don't know if this is significant, however. I'm going to ignore it for now.
 
@@ -101,78 +101,6 @@ process.jetCorrection = cms.Sequence( process.patJetCorrFactorsUpdatedJEC * proc
 #                           isData=True
 #                           )
 
-# Now you are creating the bad muon corrected MET
-#process.load('RecoMET.METFilters.badGlobalMuonTaggersMiniAOD_cff')
-#process.badGlobalMuonTaggerMAOD.taggingMode = cms.bool(True)
-#process.cloneGlobalMuonTaggerMAOD.taggingMode = cms.bool(True)
-
-#from PhysicsTools.PatUtils.tools.muonRecoMitigation import muonRecoMitigation
-
-#muonRecoMitigation(
-#    process = process,
-#    pfCandCollection = "packedPFCandidates", #input PF Candidate Collection
-#    runOnMiniAOD = True, #To determine if you are running on AOD or MiniAOD
-#    selection="", #You can use a custom selection for your bad muons. Leave empty if you would like to use the bad muon recipe definition.
-#    muonCollection="", #The muon collection name where your custom selection will be applied to. Leave empty if you would like to use the bad muon recipe definition.
-#    cleanCollName="cleanMuonsPFCandidates", #output pf candidate collection ame
-#    cleaningScheme="computeAllApplyClone", #Options are: "all", "computeAllApplyBad","computeAllApplyClone". Decides which (or both) bad muon collections to be used for MET cleaning coming from the bad muon recipe.
-#    postfix="" #Use if you would like to add a post fix to your muon / pf collections
-#    )
-
-#runMetCorAndUncFromMiniAOD(process,
-#                           isData=True,
-#                           pfCandColl="cleanMuonsPFCandidates",
-#                           recoMetFromPFCs=True,
-#                           postfix="MuClean"
-#                           )
-
-#process.mucorMET = cms.Sequence(                     
-#    process.badGlobalMuonTaggerMAOD *
-#    process.cloneGlobalMuonTaggerMAOD *
-#    #process.badMuons * # If you are using cleaning mode "all", uncomment this line
-#    process.cleanMuonsPFCandidates *
-#    process.fullPatMetSequenceMuClean
-#    )
-
-# Now you are creating the e/g corrected MET on top of the bad muon corrected MET (on re-miniaod)
-#from PhysicsTools.PatUtils.tools.corMETFromMuonAndEG import corMETFromMuonAndEG
-#corMETFromMuonAndEG(process,
-#                    pfCandCollection="", #not needed                                                                                                                                                                                                                                                                                        
-#                    electronCollection="slimmedElectronsBeforeGSFix",
-#                    photonCollection="slimmedPhotonsBeforeGSFix",
-#                    corElectronCollection="slimmedElectrons",
-#                    corPhotonCollection="slimmedPhotons",
-#                    allMETEGCorrected=True,
-#                    muCorrection=False,
-#                    eGCorrection=True,
-#                    runOnMiniAOD=True,
-#                    postfix="MuEGClean"
-#                    )
-
-#process.slimmedMETsMuEGClean = process.slimmedMETs.clone()
-#process.slimmedMETsMuEGClean.src = cms.InputTag("patPFMetT1MuEGClean")
-#process.slimmedMETsMuEGClean.rawVariation =  cms.InputTag("patPFMetRawMuEGClean")
-#process.slimmedMETsMuEGClean.t1Uncertainties = cms.InputTag("patPFMetT1%sMuEGClean")
-#
-#del process.slimmedMETsMuEGClean.caloMET
-
-# If you are running in the scheduled mode:
-#process.egcorrMET = cms.Sequence(
-#    process.cleanedPhotonsMuEGClean+process.cleanedCorPhotonsMuEGClean+
-#    process.matchedPhotonsMuEGClean + process.matchedElectronsMuEGClean +
-#    process.corMETPhotonMuEGClean+process.corMETElectronMuEGClean+
-#    process.patPFMetT1MuEGClean+process.patPFMetRawMuEGClean+
-#    process.patPFMetT1SmearMuEGClean+process.patPFMetT1TxyMuEGClean+
-#    process.patPFMetTxyMuEGClean+process.patPFMetT1JetEnUpMuEGClean+
-#    process.patPFMetT1JetResUpMuEGClean+process.patPFMetT1SmearJetResUpMuEGClean+
-#    process.patPFMetT1ElectronEnUpMuEGClean+process.patPFMetT1PhotonEnUpMuEGClean+
-#    process.patPFMetT1MuonEnUpMuEGClean+process.patPFMetT1TauEnUpMuEGClean+
-#    process.patPFMetT1UnclusteredEnUpMuEGClean+process.patPFMetT1JetEnDownMuEGClean+
-#    process.patPFMetT1JetResDownMuEGClean+process.patPFMetT1SmearJetResDownMuEGClean+
-#    process.patPFMetT1ElectronEnDownMuEGClean+process.patPFMetT1PhotonEnDownMuEGClean+
-#    process.patPFMetT1MuonEnDownMuEGClean+process.patPFMetT1TauEnDownMuEGClean+
-#    process.patPFMetT1UnclusteredEnDownMuEGClean+process.slimmedMETsMuEGClean)
-
 
 ###############################
 ######### MET Filter ##########
@@ -215,6 +143,7 @@ process.makeTopologyNtupleMiniAOD.bDiscCut=cms.double(-1.0)
 process.makeTopologyNtupleMiniAOD.minElePt=cms.double(9.0)
 process.makeTopologyNtupleMiniAOD.maxEleEta=cms.double(2.7)
 process.makeTopologyNtupleMiniAOD.eleRelIso=cms.double(0.50)
+process.makeTopologyNtupleMiniAOD.maxMuonEta=cms.double(2.8)
 process.makeTopologyNtupleMiniAOD.maxInvMuMuMass=cms.double(10.0)
 process.makeTopologyNtupleMiniAOD.maxMuonEta=cms.double(2.8)
 process.makeTopologyNtupleMiniAOD.minMuonPt1=cms.double(15.0)
@@ -225,7 +154,6 @@ process.makeTopologyNtupleMiniAOD.maxDcotForPhotonRej=cms.double(0.03)
 process.makeTopologyNtupleMiniAOD.fillAll=cms.bool(True)
 #process.makeTopologyNtupleMiniAOD.btagParameterizationList = cms.vstring()
 #process.makeTopologyNtupleMiniAOD.btagParameterizationMode = cms.vstring()
-
 #Don't actually do cuts
 process.makeTopologyNtupleMiniAOD.doCuts=cms.bool(True) # if set to false will skip ALL cuts. Z veto still applies electron cuts.
 
@@ -240,7 +168,7 @@ process.makeTopologyNtupleMiniAOD.conversionsToken = cms.InputTag("reducedEgamma
 
 ## Source
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring()
+    fileNames = cms.untracked.vstring(options.inputFiles)
 )
 
 ## Maximal Number of Events
@@ -271,7 +199,7 @@ process.out.outputCommands += cms.untracked.vstring('keep *_flavorHistoryFilter_
 process.out.fileName = cms.untracked.string('Data_out.root')
 
 #NTuple output
-process.TFileService = cms.Service("TFileService", fileName = cms.string('Data_test.root') )
+process.TFileService = cms.Service("TFileService", fileName = cms.string (options.outputFile) )
 process.options.wantSummary = False
 process.out.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('p'))
 
