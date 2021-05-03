15e7a5fe315faeb57573f671579bd7e134159bc7
davidcarbonis <alexander.david.morton@cern.ch>
2021-05-03 17:51:59 +0100

    Removed redundant files, but kept a few examples

--- generation/test/2017_scripts/ggH_MS2_ctauS10_example/NLO_ggHToSS_MH125_SinclusiveFiltered_2017.py
+++ generation/test/2017_scripts/ggH_MS2_ctauS10_example/NLO_ggHToSS_MH125_SinclusiveFiltered_2017.py
@@ -31,8 +31,8 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 options = VarParsing.VarParsing ('analysis')
 options.parseArguments()
 
-intEvts  =  cms.untracked.int32(1000)
-uintEvts = cms.untracked.uint32(1000)
+intEvts  =  cms.untracked.int32(5000)
+uintEvts = cms.untracked.uint32(5000)
 
 process.maxEvents = cms.untracked.PSet(
     input = intEvts
@@ -92,10 +92,13 @@ process.generator = cms.EDFilter("Pythia8HadronizerFilter",
         processParameters = cms.vstring('POWHEG:nFinal = 1', 
 #            <particle id="..." name, antiName, spinType=, chargeType, colType, m0, mWidth, mMin, mMax, tau0> ##tau0 = nominal proper lifetime (mm/c) 
 #            '9000006:all = sk   skbar    0        0          0       2.0  0.0197327e-11/tau0  1.0  75.0 tau0', 
-            '9000006:all = sk   skbar    0        0          0       1.0  1.9732e-14  1.0  75.0 10',
-            '9000006:oneChannel = 1  1.0 101  11 -11',
-            '9000006:addChannel = 1  1.0 101  13 -13',
-            '9000006:addChannel = 1  1.0 101  15 -15',
+            '9000006:all = sk   skbar    0        0          0       2.0  1.9732e-14  1.0  75.0 10',
+            '9000006:oneChannel = 1  1.0 101  13 -13',
+            '9000006:addChannel = 3  1.0 101  1 -1',
+            '9000006:addChannel = 3  1.0 101  2 -2',
+            '9000006:addChannel = 3  1.0 101  3 -3',
+            '9000006:addChannel = 3  1.0 101  4 -4',
+            '9000006:addChannel = 3  1.0 101  5 -5',
             '9000006:mayDecay = on',
             '9000006:isResonance = on',
             '25:m0 = 125.0',
@@ -103,9 +106,12 @@ process.generator = cms.EDFilter("Pythia8HadronizerFilter",
             '25:addChannel = 1 0.000000001 101 9000006 -9000006',
             '25:onIfMatch = 9000006 -9000006',
             '9000006:onMode = off',
-            '9000006:onIfAny = 11',
-            '9000006:onIfAny = 13',
-            '9000006:onIfAny = 15'),
+            '9000006:onNegIfAny = 1',
+            '9000006:onNegIfAny = 2',
+            '9000006:onNegIfAny = 3',
+            '9000006:onNegIfAny = 4',
+            '9000006:onNegIfAny = 5',
+            '9000006:onIfAny = 13'),
         pythia8CP2Settings = cms.vstring('Tune:pp 14', 
             'Tune:ee 7', 
             'MultipartonInteractions:ecmPow=0.1391', 
@@ -145,6 +151,16 @@ process.generator = cms.EDFilter("Pythia8HadronizerFilter",
 )
 
 
+process.genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+                                       saveBarCodes = cms.untracked.bool(True),
+                                       src = cms.InputTag("generator", "unsmeared"),
+                                       abortOnUnknownPDGCode = cms.untracked.bool(False)
+                                       )
+
+process.scalarDecayFilter = cms.EDFilter("MCScalarDecayFilter",
+                           filterAlgoPSet = cms.PSet(genParticles = cms.InputTag("genParticlesForFilter"))
+                           )
+
 process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
     args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/powheg/V2/gg_H_quark-mass-effects_NNPDF31_13TeV_M125/v1/gg_H_quark-mass-effects_NNPDF31_13TeV_M125_slc6_amd64_gcc630_CMSSW_9_3_0.tgz'),
     nEvents = uintEvts,
@@ -154,7 +170,8 @@ process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
 )
 
 
-process.ProductionFilterSequence = cms.Sequence(process.generator)
+process.ProductionFilterSequence = cms.Sequence(process.generator * (process.genParticlesForFilter + process.scalarDecayFilter))
+#process.ProductionFilterSequence = cms.Sequence(process.generator)
 
 # Path and EndPath definitions
 process.lhe_step = cms.Path(process.externalLHEProducer)
